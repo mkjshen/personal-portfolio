6fa0ac669e4094a32f8406102b25efb06af8897e
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-07-14 15:07:18 +0800

    removed resume for now

--- app/components/SocialLinks.tsx
+++ app/components/SocialLinks.tsx
@@ -32,8 +32,9 @@ const SocialLinks = () => {
         </>
       ),
       href: "mailto:mitchel.shen@gmail.com",
+      style: "rounded-br-mid",
     },
-    {
+    /* {
       id: 4,
       child: (
         <>
@@ -43,20 +44,20 @@ const SocialLinks = () => {
       href: "/resume.pdf",
       style: "rounded-br-mid",
       download: true,
-    },
+    }, */
   ];
 
   return (
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        {links.map(({ id, child, href, style, download }) => (
+        {links.map(({ id, child, href, style }) => (
           <li
             key={id}
             className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style}`}>
             <a
               href={href}
               className="flex justify-between items-center w-full text-white"
-              download={download}
+              // download={download}
               target="_blank"
               rel="noreferrer">
               {child}
